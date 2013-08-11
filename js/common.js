321d9235fbe299b0d0c0e34ba6c9075e84973535
David Kao <david8018d@gmail.com>
2013-08-12 06:05:26 +0800

    intro site
    ver 1

---
+++ js/common.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+					$('.hov75').hover(function(){$(this).find('img').fadeTo(120,0.75)},function(){$(this).find('img').fadeTo(60,1)});
+					
+					//visual animation
+					$('.mark img').hover(
+						function(){
+							$(this).parents(1).find('.visual_back2').stop().fadeTo(220,1);$(this).parents(1).find('h1.mark').stop().fadeTo(100,0.1);
+							$(this).parents(1).find('.mark.pos2').stop().animate({'marginTop':'9px','marginLeft':'-9px','opacity': 0.14},100);
+							$(this).parents(1).find('.mark.pos3').stop().animate({'marginTop':'-9px','marginLeft':'9px','opacity': 0.14},140);
+										},
+
+						function(){
+							$(this).parents(1).find('.visual_back2').stop().fadeTo(100,0);$(this).parents(1).find('h1.mark').stop().fadeTo(250,0.4);
+							$(this).parents(1).find('.mark.pos2').stop().animate({'marginTop':0,'marginLeft':0,'opacity': 0},100);
+							$(this).parents(1).find('.mark.pos3').stop().animate({'marginTop':0,'marginLeft':0,'opacity': 0},140);
+										}
+					)
+					
+					
+					$('.portfolio .item').each(function(i){
+						
+						if(i%2==0)
+						$(this).find('.inner .hover').addClass('green');
+						else
+						$(this).find('.inner .hover').addClass('purple');
+						})
+					
+					$('.portfolio .item .inner').hover(function(){$(this).find('.hover').fadeTo(150,0.7)},function(){$(this).find('.hover').fadeTo(60,0)});
+						   
+});
